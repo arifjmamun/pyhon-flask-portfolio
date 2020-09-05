48a1ccbd5149c49e0599f6fefdb14ded76dddacc
Md. Ariful Islam <arifjmamun24@gmail.com>
2020-09-05 12:00:43 +0600

    Implemented txt and csv file as database to store contact information

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+import csv
 from flask import Flask, render_template, redirect, request, url_for
+
+
 app = Flask(__name__)
 
 
@@ -32,11 +35,36 @@ def contact():
     return render_template('./contact.html')
 
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(
+            database2, 
+            delimiter=',', 
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL
+        )
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        print(data)
-        return redirect(url_for('contact', is_submitted=True))
+        try:            
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect(url_for('contact', is_submitted=True))            
+        except:
+            return 'did not save to database'
     else:
         return 'something went wrong! try again'
