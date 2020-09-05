f7e4e4907a8c73750b36000c29fc59f4d38f65e8
Md. Ariful Islam <arifjmamun24@gmail.com>
2020-09-05 10:33:34 +0600

    Added contact submit functionality with thankyou response

--- server.py
+++ server.py
@@ -1,20 +1,12 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
+@app.route('/index')
 def hello_world():
     return render_template('./index.html')
-
-
-@app.route('/home')
-def home():
-    return redirect('/')
-
-
-@app.route('/index')
-def index():
-    return redirect('/')
 
 
 @app.route('/about')
@@ -34,9 +26,17 @@ def work():
 
 @app.route('/contact')
 def contact():
+    is_submitted = request.args.get('is_submitted')
+    if is_submitted:
+        return render_template('./contact.html', is_submitted=is_submitted)
     return render_template('./contact.html')
 
 
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'Dogs'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect(url_for('contact', is_submitted=True))
+    else:
+        return 'something went wrong! try again'
